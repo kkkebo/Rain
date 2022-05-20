e867d36efc4ac1a0c0285b6fe086ca424fb4d93f
Andrey.K <andrey-8991@yandex.ru>
2022-05-20 15:58:51 +0300

    Setup admon-panel

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
+from django.utils.safestring import mark_safe
+
 from .models import *
 
 
@@ -12,10 +14,24 @@ class PostAdminForm(forms.ModelForm):
         fields = '__all__'
 
 
-
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     form = PostAdminForm
+    list_display = ('id', 'title', 'author', 'slug', 'category', 'created_at', 'get_photo')
+    list_display_links = ['id', 'title']
+    search_fields = ('title',)
+    list_filter = ('category', 'author')
+    save_on_top = True
+    readonly_fields = ('views', 'created_at', 'get_photo')
+    fields = ('title', 'slug', 'category', 'author', 'tags', 'content', 'photo', 'get_photo', 'views', 'created_at', )
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        return '-'
+
+    get_photo.short_description = 'Фото'
+
 
 
 class CategoryAdmin(admin.ModelAdmin):
