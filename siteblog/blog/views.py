87e08b9d20f47d6e55afedd1cbeb39c4b734a5c5
Andrey.K <andrey-8991@yandex.ru>
2022-05-23 15:27:51 +0300

    Added pagination postsbycategory view

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post, Category, Tag
+from django.views.generic import ListView
+
+from .models import Post, Category
 
 
 class Home(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -15,9 +16,23 @@ class Home(ListView):
         return context
 
 
+class PostsByCategory(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
+
+
 def get_post(request, slug):
     return render(request, 'blog/index.html')
-
 
 
 def index(request):
@@ -26,5 +41,3 @@ def index(request):
 
 def get_category(request, slug):
     return render(request, 'blog/category.html')
-
-
